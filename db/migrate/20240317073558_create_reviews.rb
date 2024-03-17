369db77976b888a387b8acd65622d59b2bcf4cbd
EC2 Default User <ec2-user@ip-172-31-37-30.ap-northeast-1.compute.internal>
2024-03-17 07:48:07 +0000

    [Add]モデル

---
+++ db/migrate/20240317073558_create_reviews.rb
@@ -0,0 +1,11 @@
+class CreateReviews < ActiveRecord::Migration[6.1]
+  def change
+    create_table :reviews do |t|
+      t.integer :customer_id,     null: false
+      t.integer :tourist_spot_id, null: false
+      t.integer :evaluation,      null: false
+      t.text :thoughts,           null: false
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20240317073632_create_tourist_spots.rb
@@ -0,0 +1,11 @@
+class CreateTouristSpots < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tourist_spots do |t|
+      t.string :name,      null: false
+      t.text :explanation, null: false
+      t.string :address,   null: false
+      t.string :season,    null: false
+      t.timestamps
+    end
+  end
+end
